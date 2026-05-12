e903c3868f17967f38552bfa1aa9ba796e8b1148
dkeraudren <damien.keraudren@gmail.com>
2026-05-12 15:47:30 +0200

    Fixed Qt import

--- nodes_operator/node_operator_mainwindow.py
+++ nodes_operator/node_operator_mainwindow.py
@@ -8,9 +8,9 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide2.QtCore import *
-from PySide2.QtGui import *
-from PySide2.QtWidgets import *
+from cgev.ui.Qt.QtCore import *
+from cgev.ui.Qt.QtGui import *
+from cgev.ui.Qt.QtWidgets import *
 
 
 class Ui_Form(object):
